21dd020a0316b8a328104ddd90d017c45588d8c4
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2016-08-11 23:14:42 -0300

    Using module's export

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
-function addPlaceholder(sticker) {
-  const positionRules = ['width', 'height', 'margin'];
-  const stickerStyle = window.getComputedStyle(sticker);
-  const placeholder = document.createElement('div');
-  for (const rule in stickerStyle) {
-    if (positionRules.includes(rule)) {
-      placeholder.style[rule] = stickerStyle[rule];
+export default {
+  addPlaceholder(sticker) {
+    const positionRules = ['width', 'height', 'margin'];
+    const stickerStyle = window.getComputedStyle(sticker);
+    const placeholder = document.createElement('div');
+    for (const rule in stickerStyle) {
+      if (positionRules.includes(rule)) {
+        placeholder.style[rule] = stickerStyle[rule];
+      }
     }
-  }
-  sticker.parentNode.insertBefore(placeholder, sticker);
-  sticker.placeholder = placeholder;
-}
-function removePlaceholder(sticker) {
-  if ( sticker.placeholder ) {
-    sticker.parentNode.removeChild(sticker.placeholder);
-    sticker.placeholder = undefined;
-  }
-}
+    sticker.parentNode.insertBefore(placeholder, sticker);
+    sticker.placeholder = placeholder;
+  },
 
-function watchSticker(evt, sticker) {
-  const { offsetHeight, bottomRef } = sticker;
-  const top = (sticker.placeholder || sticker).getBoundingClientRect().top;
-  const isAfterSticker = top <= 0;
-  const isAfterRef = bottomRef && bottomRef.getBoundingClientRect().bottom < offsetHeight;
+  removePlaceholder(sticker) {
+    if ( sticker.placeholder ) {
+      sticker.parentNode.removeChild(sticker.placeholder);
+      sticker.placeholder = undefined;
+    }
+  },
 
-  if (!sticker.placeholder && isAfterSticker && !isAfterRef) {
-    addPlaceholder(sticker);
-    sticker.classList.add('sticky');
-  }
-  else if (sticker.placeholder && (!isAfterSticker || isAfterRef)) {
-    removePlaceholder(sticker)
-    sticker.classList.remove('sticky');
-  }
-};
+  watchSticker(evt, sticker) {
+    const { offsetHeight, bottomRef } = sticker;
+    const top = (sticker.placeholder || sticker).getBoundingClientRect().top;
+    const isAfterSticker = top <= 0;
+    const isAfterRef = bottomRef && bottomRef.getBoundingClientRect().bottom < offsetHeight;
 
-function fixOnScroll(sticker) {
-  if ( sticker.stick ) {
-    return;
-  }
-  sticker.stick = (e) => watchSticker(e, sticker);
-  sticker.bottomRef = document.querySelector(sticker.getAttribute('data-fos-bottomref'));
-  window.addEventListener('scroll', sticker.stick);
-}
+    if (!sticker.placeholder && isAfterSticker && !isAfterRef) {
+      addPlaceholder(sticker);
+      sticker.classList.add('sticky');
+    }
+    else if (sticker.placeholder && (!isAfterSticker || isAfterRef)) {
+      removePlaceholder(sticker)
+      sticker.classList.remove('sticky');
+    }
+  },
 
-function discoverAll() {
-  const elements = [].slice.call(document.querySelectorAll('[data-fos]'), 0);
-  elements.forEach(fixOnScroll)
+  fixOnScroll(sticker) {
+    if ( sticker.stick ) {
+      return;
+    }
+    sticker.stick = (e) => watchSticker(e, sticker);
+    sticker.bottomRef = document.querySelector(sticker.getAttribute('data-fos-bottomref'));
+    window.addEventListener('scroll', sticker.stick);
+  },
+
+  discoverAll() {
+    const elements = [].slice.call(document.querySelectorAll('[data-fos]'), 0);
+    elements.forEach(fixOnScroll)
+  },
 }
