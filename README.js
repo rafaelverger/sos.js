32797cb20b1545d5cd9dc7f5d49335811ecb6b76
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2017-05-23 12:26:02 -0300

    Add README.md

---
+++ README.js
@@ -0,0 +1,10 @@
+# SOS.js
+### Stick elements on page's scroll
+
+```js
+// to search for all elements with "data-fos" attribute
+SOS.discoverAll()
+
+// to bind to an specific element
+SOS.stickOnScroll(document.querySelector('#elmt'));
+```
